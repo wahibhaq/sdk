8c181b100a2cf47aa6c0b21071ccb331ec434fad
wahib <wahib.tech@gmail.com>
2014-10-28 10:06:38 +0100

    bin and gen removed

--- SocketIOSampleProject/gen/com/vibewrite/socketiosampleproject/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.vibewrite.socketiosampleproject;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
